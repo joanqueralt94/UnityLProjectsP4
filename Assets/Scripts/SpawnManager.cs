711e9a1c425eb49a81577fcfe5ba8023a8e3ca38
joanqueralt94 <salnitre94@gmail.com>
2023-04-17 20:31:00 +0200

    Added enemy destroy when fall off + spawn manager wave control

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,33 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemy;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
